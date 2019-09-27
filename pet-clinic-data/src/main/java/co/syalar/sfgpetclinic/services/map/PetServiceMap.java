26d50822660371977097c825cbc210abccccdee8
Juan David Rodriguez Romero <jd.rodriguez@globant.com>
2019-09-27 12:35:01 -0500

    Implement map base services.  closes #6

---
+++ pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,36 @@
+package co.syalar.sfgpetclinic.services.map;
+
+import co.syalar.sfgpetclinic.model.Pet;
+import co.syalar.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+/**
+ * Created by jd.rodriguez
+ */
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
