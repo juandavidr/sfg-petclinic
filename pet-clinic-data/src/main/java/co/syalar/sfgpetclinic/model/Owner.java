82445d73b2df696695f657164f996db85ad72a27
Juan David Rodriguez Romero <jd.rodriguez@globant.com>
2019-10-21 12:41:22 -0500

    added visits updated owner with pet relationship Closes #26

--- pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/model/Owner.java
@@ -1,7 +1,18 @@
 package co.syalar.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by jd.rodriguez
  */
 public class Owner extends Person {
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
