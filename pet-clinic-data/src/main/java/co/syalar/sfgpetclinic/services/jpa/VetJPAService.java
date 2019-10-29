e571b32067d04a65dd794a50f9b0eee13ec3e57e
Juan David Rodriguez Romero <jd.rodriguez@globant.com>
2019-10-29 12:30:58 -0500

    Added Spring data jpa service for vets. Closes #41.

---
+++ pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/services/jpa/VetJPAService.java
@@ -0,0 +1,51 @@
+package co.syalar.sfgpetclinic.services.jpa;
+
+import co.syalar.sfgpetclinic.model.Vet;
+import co.syalar.sfgpetclinic.repositories.VetRepository;
+import co.syalar.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by jd.rodriguez
+ */
+@Service
+@Profile("jpa")
+public class VetJPAService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetJPAService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
