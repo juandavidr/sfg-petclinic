638d3ce514d7ad3ef5fd0138bf1f6e759e61500c
Juan David Rodriguez Romero <jd.rodriguez@globant.com>
2019-10-29 11:40:35 -0500

    Implemented mapped super class baseentity. Closes #35.

--- pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/model/BaseEntity.java
@@ -1,11 +1,18 @@
 package co.syalar.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by jd.rodriguez
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
