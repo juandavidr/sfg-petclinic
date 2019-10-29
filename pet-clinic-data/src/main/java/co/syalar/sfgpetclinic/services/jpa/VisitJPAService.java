623b59345a1de506be1535866891a74287b7a3a6
Juan David Rodriguez Romero <jd.rodriguez@globant.com>
2019-10-29 13:10:04 -0500

    Implemented jpa impl for visits.  Closes #47.

---
+++ pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/services/jpa/VisitJPAService.java
@@ -0,0 +1,51 @@
+package co.syalar.sfgpetclinic.services.jpa;
+
+import co.syalar.sfgpetclinic.model.Visit;
+import co.syalar.sfgpetclinic.repositories.VisitRepository;
+import co.syalar.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by jd.rodriguez
+ */
+@Service
+@Profile("jpa")
+public class VisitJPAService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitJPAService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
--- pet-clinic-web/src/main/java/co/syalar/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/co/syalar/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package co.syalar.sfgpetclinic.bootstrap;
 
 import co.syalar.sfgpetclinic.model.*;
-import co.syalar.sfgpetclinic.services.OwnerService;
-import co.syalar.sfgpetclinic.services.PetTypeService;
-import co.syalar.sfgpetclinic.services.SpecialtyService;
-import co.syalar.sfgpetclinic.services.VetService;
+import co.syalar.sfgpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -20,13 +17,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService,
-            PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
+            SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -92,6 +91,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy kitty");
+        visitService.save(catVisit);
+
         System.out.println("Loaded owners..." + ownerService.findAll().size());
 
         Vet vet1 = new Vet();
