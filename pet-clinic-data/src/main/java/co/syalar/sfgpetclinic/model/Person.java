08bdd1968b628ea9e95e23edb329cc19b192c2a8
Juan David Rodriguez Romero <jd.rodriguez@globant.com>
2019-10-29 11:49:53 -0500

    updated owner with jpa entity mappings. Closes #36.

--- pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/model/Person.java
@@ -1,10 +1,16 @@
 package co.syalar.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 /**
  * Created by jd.rodriguez
  */
-public class Person extends BaseEntity{
+@MappedSuperclass
+public class Person extends BaseEntity {
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
--- pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/model/PetType.java
@@ -1,9 +1,16 @@
 package co.syalar.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by jd.rodriguez
  */
-public class PetType extends BaseEntity{
+@Entity
+@Table(name = "types")
+public class PetType extends BaseEntity {
+    @Column(name = "name")
     private String name;
 
     public String getName() {
