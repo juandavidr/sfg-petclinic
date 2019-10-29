166dc94ba4e1cae050889743aa089ce7b74abe92
Juan David Rodriguez Romero <jd.rodriguez@globant.com>
2019-10-29 13:02:54 -0500

    Implemented map impl for visits.  Closes #46.

---
+++ pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package co.syalar.sfgpetclinic.services;
+
+import co.syalar.sfgpetclinic.model.Visit;
+
+/**
+ * Created by jd.rodriguez
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,50 @@
+package co.syalar.sfgpetclinic.services.map;
+
+import co.syalar.sfgpetclinic.model.Visit;
+import co.syalar.sfgpetclinic.services.PetService;
+import co.syalar.sfgpetclinic.services.VisitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by jd.rodriguez
+ */
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    private PetService petService;
+
+    @Autowired
+    public VisitMapService(PetService petService) {
+        this.petService = petService;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
