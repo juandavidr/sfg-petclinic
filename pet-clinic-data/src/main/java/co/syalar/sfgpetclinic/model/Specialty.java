a20bec854f41bc65094d8fc389aa20ca4e7ab068
Juan David Rodriguez Romero <jd.rodriguez@globant.com>
2019-10-29 11:54:47 -0500

    updated vet with jpa entity mappings. Closes #37.

--- pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/model/Specialty.java
@@ -1,9 +1,17 @@
 package co.syalar.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by jd.rodriguez
  */
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/co/syalar/sfgpetclinic/model/Vet.java
@@ -1,12 +1,19 @@
 package co.syalar.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by jd.rodriguez
  */
+@Entity
+@Table(name = "vet")
 public class Vet extends Person {
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
