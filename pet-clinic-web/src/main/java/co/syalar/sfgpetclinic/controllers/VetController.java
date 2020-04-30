81a68de16a7288ea5221ab4c5acfd10eb25e6b4f
Juan David Rodriguez Romero <jd.rodriguez@globant.com>
2020-04-29 22:31:00 -0500

    Added json endpoint  closes #65

--- pet-clinic-web/src/main/java/co/syalar/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/co/syalar/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,19 @@
 package co.syalar.sfgpetclinic.controllers;
 
+import co.syalar.sfgpetclinic.model.Vet;
 import co.syalar.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * Created by jd.rodriguez
  */
-@RequestMapping({"/vets","/vets.html"})
+
 @Controller
 public class VetController {
     private final VetService vetService;
@@ -17,9 +22,14 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({ "", "/", "/index", "/index.html" })
+    @RequestMapping({ "/vets", "/vets.html", "/vets/index", "/vets/index.html" })
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
